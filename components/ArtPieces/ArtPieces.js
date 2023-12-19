6fee47d0874b71bcb5e2764c0ed4d16426e58720
Burak Yüksel <burak.yueksel@hotmail.com>
2023-12-19 13:21:48 +0100

    add css

--- components/ArtPieces/ArtPieces.js
+++ components/ArtPieces/ArtPieces.js
@@ -1,33 +1,42 @@
 import ArtPiecePreview from "../ArtPiecePreview/ArtPiecePreview";
 import Link from "next/link";
 import FavoriteButton from "../FavoriteButton/FavoriteButton";
+
 export default function ArtPieces({
   artPiecesInfo,
   onToggleFavorite,
   onSubmitComment,
 }) {
   return (
-    <ul>
-      {Array.isArray(artPiecesInfo) &&
-        artPiecesInfo.map((piece) => {
-          return (
-            <li key={piece.slug}>
-              <Link href={`/art-pieces/${piece.slug}`}>
-                <ArtPiecePreview
-                  image={piece.imageSource}
-                  title={piece.name}
-                  artist={piece.artist}
-                  genre={piece.genre}
-                  onSubmitComment={onSubmitComment}
-                />
-              </Link>
-              <FavoriteButton
-                onToggleFavorite={() => onToggleFavorite(piece.slug)}
-                isFavorite={piece.isFavorite}
-              ></FavoriteButton>
-            </li>
-          );
-        })}
-    </ul>
+    <div className="art-pieces-div">
+      <ul className="art-pieces-ul">
+        {Array.isArray(artPiecesInfo) &&
+          artPiecesInfo.map((piece) => {
+            return (
+              <li key={piece.slug} className="art-pieces-list">
+                <div className="art-piece-container">
+                  <FavoriteButton
+                    onToggleFavorite={() => onToggleFavorite(piece.slug)}
+                    isFavorite={piece.isFavorite}
+                    className="favicon"
+                  />
+                  <Link
+                    href={`/art-pieces/${piece.slug}`}
+                    className="art-pieces-link"
+                  >
+                    <ArtPiecePreview
+                      image={piece.imageSource}
+                      title={piece.name}
+                      artist={piece.artist}
+                      genre={piece.genre}
+                      onSubmitComment={onSubmitComment}
+                    />
+                  </Link>
+                </div>
+              </li>
+            );
+          })}
+      </ul>
+    </div>
   );
 }
--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -3,15 +3,17 @@ import Link from "next/link";
 export default function Navigation() {
   return (
     <>
-      <ul>
-        <li>
+      <ul className="nav-container">
+        <li className="nav-li-element">
           <Link href="/">Spotlight</Link>
         </li>
-        <li>
+        <li className="nav-li-element">
           <Link href="/art-pieces">Pieces</Link>
         </li>
-        <li>
-          <Link href="/favorites">FavoUrite</Link>
+        <li className="nav-li-element">
+          <Link className="nav-li-link" href="/favorites">
+            FavoUrite
+          </Link>
         </li>
       </ul>
     </>
